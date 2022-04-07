fcee8af63463a00772a3b9e7ed4fa5cbc16e3731
Rowan-Paul <contact@rowanpaulflynn.com>
2022-04-07 15:35:36 +0200

    Add redirecting new users to two factor

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,6 +6,7 @@ import prisma from '../../../lib/prisma';
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
+  pages: { newUser: '/twofactor' },
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER,
@@ -13,10 +14,15 @@ export default NextAuth({
     })
   ],
   callbacks: {
-    async session({ session, user }: any) {
+    async session({ session, user }) {
       session.user.id = user.id;
       session.user.twofactor = !!user.twofactor;
       return Promise.resolve(session);
+    }
+  },
+  events: {
+    async signIn({ user }) {
+      console.log(user);
     }
   },
   secret: process.env.NEXTAUTH_SECRET
